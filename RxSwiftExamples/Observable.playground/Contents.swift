8d86c477fd9ea9d81dbe5ff2d65e323f6ce7c1c3
Lyunho Kim <kimlh2@gmail.com>
2019-08-25 16:48:28 +0900

    example init

--- RxSwiftExamples/Observable.playground/Contents.swift
+++ RxSwiftExamples/Observable.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+
+
+
+//let observable = Observable.from([1, 2, 3])
+//
+//observable.subscribe(onNext: { element in
+//    print(element)
+//})
+
+
+
+//var nums = [1, 2, 3]
+//var index = 0
+//for _ in nums {
+//    print(nums[index])
+//    nums = [4, 5, 6]
+//}
+//print(nums)
+
+
+var nums = [1, 2, 3]
+for i in nums {
+    print(i)
+    nums = [4, 5, 6]
+}
+print(nums)
+
+
